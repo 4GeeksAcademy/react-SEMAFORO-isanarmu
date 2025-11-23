cc981dad8d6898187de0773af03af5cca4f5f25b
Irene <isanarmu@gmail.com>
2025-11-23 10:48:16 +0000

    semaforo terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const [activo, setActivo] = useState("")
+	const [light, setLight] = useState("")
+
+	const colorActivo = (color) => {
+		if (activo === color) {
+			setActivo("")
+		} else {
+			setActivo(color);
+		}
+	}
+
+	const lightOn = () => setLight(true);
+	const lightOff = () => setLight(false);
+
+	return (
+		<>
+			<div className="poste"> </div>
+			<div className="box">
+				<div className="container">
+					<span className={(light || activo == 'rojo') ? 'rojo rojo-activo' : 'rojo'} onClick={() => colorActivo("rojo")} ></span>
+					<span className={(light || activo == 'amarillo') ? 'amarillo amarillo-activo' : 'amarillo'} onClick={() => colorActivo("amarillo")}></span>
+					<span className={(light || activo == 'verde') ? 'verde verde-activo' : 'verde'} onClick={() => colorActivo("verde")}></span>
+					<span className={(light || activo == 'morado') ? 'morado morado-activo' : 'morado'} onClick={() => colorActivo("morado")}></span>
+				</div>
+
+			</div>
+			<div className="botones">
+				<button onClick={lightOn}>On</button>
+				<button onClick={lightOff}>Off</button>
+				<button onClick={() => colorActivo("morado")}>Purple</button>
+			</div>
+		</>
 	);
 };
 
